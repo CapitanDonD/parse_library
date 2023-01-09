50e75136e77f80f190cdaf16cfa85cfc2d89c1e9
CapitanDonD <79704733+CapitanDonD@users.noreply.github.com>
2023-01-09 20:01:01 +0700

    new function: finding_content

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import os
 from pathlib import Path
 
+from bs4 import BeautifulSoup
 import requests
+
+
+def finding_content(response):
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find(id="content").find('h1').text
+    title_book, author_name = title_tag.split(' \xa0 :: \xa0 ')
+
+    return title_book, author_name
 
 
 def check_for_redirect(response):
@@ -16,16 +25,19 @@ def save_book(number_book, response):
 
 def main():
     Path("books").mkdir(parents=True, exist_ok=True)
-
     for number_book in range(1, 11):
+        page_book_url = f'https://tululu.org/b{number_book}/'
         params = {'id': number_book}
         book_txt_url = 'https://tululu.org/txt.php'
         response = requests.get(book_txt_url, params=params)
         try:
             response.raise_for_status()
             check_for_redirect(response)
-
             save_book(number_book, response)
+
+            response = requests.get(page_book_url)
+            title_book, author_name = finding_content(response)
+            print(f'Заголовок: {title_book}\nАвтор: {author_name}')
         except requests.exceptions.HTTPError:
             print(f'Книги {number_book} не существует')
 
