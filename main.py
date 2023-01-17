8b019c0e43fbdadf7d16df415a9a3bd9bf4bfa96
CapitanDonD <79704733+CapitanDonD@users.noreply.github.com>
2023-01-17 20:41:53 +0700

    download books with titles

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 from bs4 import BeautifulSoup
 import requests
+from pathvalidate import sanitize_filename
 
 
 def finding_content(response):
@@ -18,8 +19,13 @@ def check_for_redirect(response):
         raise requests.exceptions.HTTPError
 
 
-def save_book(number_book, response):
-    with open(f'books/book_{number_book}.txt', "w", encoding='UTF-8') as file:
+def download_txt(url, book_filename, folder='books/'):
+    response = requests.get(url)
+    response.raise_for_status()
+    book_filename = sanitize_filename(book_filename)
+    filename = f'{book_filename}.txt'
+    path = os.path.join(folder, filename)
+    with open(path, "w", encoding='UTF-8') as file:
         file.write(response.text)
 
 
@@ -33,11 +39,12 @@ def main():
         try:
             response.raise_for_status()
             check_for_redirect(response)
-            save_book(number_book, response)
 
             response = requests.get(page_book_url)
             title_book, author_name = finding_content(response)
-            print(f'Заголовок: {title_book}\nАвтор: {author_name}')
+            numbered_title_book = f'{number_book}.{title_book}'
+            download_txt(page_book_url, numbered_title_book)
+
         except requests.exceptions.HTTPError:
             print(f'Книги {number_book} не существует')
 
